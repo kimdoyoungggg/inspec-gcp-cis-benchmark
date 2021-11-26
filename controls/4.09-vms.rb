0998c6c1c96c573ca9c1d0f6c6b40c04121704c7
kimdoyoungggg <pukasun08@gmail.com>
2021-11-26 10:57:05 +0900

    test 5

--- controls/4.09-vms.rb
+++ controls/4.09-vms.rb
@@ -49,7 +49,7 @@ title 'Ensure that Compute instances do not have public IP addresses'
   
     gce_instances.each do |instance|
       describe google_compute_instance(project: gcp_project_id, zone: instance[:zone], name: instance[:name]) do
-        its('network_interfaces_count'){should eq 0}
+        its('properties.network_interfaces.access_configs.nat_ip'){should exist}
       end
     end
   end
